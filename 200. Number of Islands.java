c700ce8d094a47332dac53860cc6c1c682cbd52f
RuixuanDai <766638145@qq.com>
2016-08-25 20:48:00 +0800

    Create 200. Number of Islands.java

---
+++ 200. Number of Islands.java
@@ -0,0 +1,29 @@
+public class Solution {
+ public int numIslands(char[][] grid) {
+			int count = 0;
+			for(int i=0 ; i<grid.length ; i++)
+			{
+				for(int j=0 ; j<grid[0].length ; j++)
+				{
+					if(grid[i][j]=='1'){
+						searchis(grid,i,j);
+						count++;
+					}				
+				}
+			}
+			return count;
+		}
+
+		private void searchis(char[][] grid, int i, int j) {
+			// TODO Auto-generated method stub
+			if(i<0||j<0||i>grid.length-1
+					||j>grid[0].length-1||grid[i][j]=='0') return;
+			grid[i][j] = '0';
+			searchis(grid,i-1,j);
+			searchis(grid,i,j-1);
+			searchis(grid,i+1,j);
+			searchis(grid,i,j+1);
+			
+			
+		}
+	}
